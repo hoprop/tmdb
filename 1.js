382a2e7a47bf66e50a4a01c818619b57f807ddfd
hoprop <162745074+hoprop@users.noreply.github.com>
2025-02-25 16:48:02 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,51 +1,52 @@
 (function () {
     'use strict';
 
-    var enableProxy = true;
-
-    var tmdb = {
-        name: 'TMDB My Proxy',
-        version: '1.0.1',
-        description: 'Проксирование постеров и API сайта TMDB',
-        path_image: 'https://img.hoprop.xyz/',
-        path_api: 'https://tmdb.hoprop.xyz/3/'
+    var tmdb_proxy = {
+      name: 'TMDB Proxy',
+      version: '1.0.4',
+      description: 'Proxying TMDB Posters and API',
+      path_image: 'https://img.hoprop.xyz/',  //  Image Proxy
+      path_api: 'https://tmdb.hoprop.xyz/'  //  API Proxy
     };
 
-    if (enableProxy) {
-        Lampa.TMDB.image = function (url) {
-            var base = 'https://image.tmdb.org/t/p/' + url;
-            return Lampa.Storage.field('proxy_tmdb') ? tmdb.path_image + 't/p/' + url : base;
-        };
-
-        Lampa.TMDB.api = function (url) {
-            var base = 'https://api.themoviedb.org/3/' + url;
-            return Lampa.Storage.field('proxy_tmdb') ? tmdb.path_api.replace(/\/3\/$/, '/') + url : base;
-        };
-
-        Lampa.Settings.listener.follow('open', function (e) {
-            if (e.name === 'tmdb') {
-                e.body.find('[data-parent="proxy"]').remove();
-            }
-        });
-    } else {
-        var plugins = Lampa.Storage.get('plugins', '[]');
-        var pluginsUpdate = false;
-        var addCubProxy = !plugins.some(p => p.url.includes('cub.red/plugin/tmdb-proxy'));
-
-        plugins = plugins.filter(p => !p.url.includes('.hoprop.xyz/tmdb.js')); // Remove old proxy
-
-        if (addCubProxy) {
-            plugins.unshift({
-                url: 'https://cub.red/plugin/tmdb-proxy',
-                status: 1,
-                name: 'TMDB Proxy',
-                author: '@lampa'
-            });
-            pluginsUpdate = true;
-        }
-
-        if (pluginsUpdate) {
-            Lampa.Storage.set('plugins', plugins);
-        }
+    function filter(u) {
+      var s = u.slice(0, 8);
+      var e = u.slice(8).replace(/\/+/g, '/');
+      return s + e;
     }
+
+    function email() {
+      return Lampa.Storage.get('account', '{}').email || '';
+    }
+
+    Lampa.TMDB.image = function (url) {
+      var base = 'https://image.tmdb.org/' + url;
+      return Lampa.Utils.addUrlComponent(filter(Lampa.Storage.field('proxy_tmdb') ? tmdb_proxy.path_image + url : base), 'email=' + encodeURIComponent(email()));
+    };
+
+    Lampa.TMDB.api = function (url) {
+      var base = 'https://api.themoviedb.org/3/' + url;
+      var proxyUrl = tmdb_proxy.path_api + url;
+
+      // Fix for missing genres
+      if (url.includes('/genre/movie/list')) {
+        console.log(" Fetching Genre List:", proxyUrl);
+      }
+
+      // Fix for search
+      if (url.includes('/search/movie') || url.includes('/search/tv')) {
+        console.log(" Search Request:", proxyUrl);
+      }
+
+      return Lampa.Utils.addUrlComponent(filter(Lampa.Storage.field('proxy_tmdb') ? proxyUrl : base), 'email=' + encodeURIComponent(email()));
+    };
+
+    Lampa.Settings.listener.follow('open', function (e) {
+      if (e.name == 'tmdb') {
+        e.body.find('[data-parent="proxy"]').remove();
+      }
+    });
+
+    console.log('TMDB-Proxy', 'started, enabled:', Lampa.Storage.field('proxy_tmdb'));
+
 })();
